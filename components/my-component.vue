35f4c3f5943864f2d6bd65d33f2e0fab9bfe3106
zzz <2395436684@qq.com>
2020-06-18 18:00:17 +0800

    修改-不成功

---
+++ components/my-component.vue
@@ -0,0 +1,25 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-18 16:12:00
+ * @LastEditTime: 2020-06-18 17:57:55
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \bpsp-uie:\doit\html-vue\components\my-components.vue
+--> 
+
+<template>
+    <div class="hello">
+        Hello {{who}}
+    </div>
+</template>
+ 
+<script>
+    module.exports={
+        name: 'myComponent',
+        data: function () {
+            return {
+                who: 'world'
+            }
+        }
+    }
+</script>
