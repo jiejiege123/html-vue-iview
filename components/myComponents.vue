309e3f97abb530343eaa2f9e1ae3a93cf84a8652
zzz <2395436684@qq.com>
2020-06-18 17:41:29 +0800

    first commit

---
+++ components/myComponents.vue
@@ -0,0 +1,20 @@
+<!--
+ * @Author: your name
+ * @Date: 2020-06-18 16:12:00
+ * @LastEditTime: 2020-06-18 16:12:01
+ * @LastEditors: Please set LastEditors
+ * @Description: In User Settings Edit
+ * @FilePath: \bpsp-uie:\doit\html-vue\components\my-components.vue
+--> 
+<template>
+  <Slider v-model="value" range />
+</template>
+<script>
+export default {
+  data() {
+    return {
+      value: [20, 50]
+    };
+  }
+};
+</script>
